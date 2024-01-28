a69aa2f3fb211c1939420c310d6312588f9b08af
Elena-Amelia <amelia013@ya.ru>
2024-01-28 19:34:23 +0400

    feat: add solution in fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import fs from "node:fs";
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filesPath = path.join(__dirname, "files");
+const filesCopyPath = path.join(__dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  fs.readdir(filesPath, (err, files) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      createCopyFolder();
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(filesPath, file),
+          path.join(filesCopyPath, file),
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      });
+    }
+  });
 };
+
+function createCopyFolder() {
+  fs.mkdir(filesCopyPath, (err) => {
+    if (err) throw new Error("FS operation failed");
+    // console.log("Folder was created");
+  });
+}
 
 await copy();
