c6005539f7124729edec48111942b9047ab71751
Elena-Amelia <amelia013@ya.ru>
2024-01-29 02:40:10 +0400

    feat: add solution in streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { exit } from "node:process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+const output = fs.createWriteStream(filePath);
+
 const write = async () => {
-    // Write your code here 
+  process.stdin.on("data", (chunk) => {
+    output.write(chunk);
+  });
+  process.stdin.on("error", (err) => process.stderr.write(err.message));
 };
 
 await write();
