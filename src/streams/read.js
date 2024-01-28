febdc16374e9dd213f4a38a5c34d7afb74171322
Elena-Amelia <amelia013@ya.ru>
2024-01-29 01:46:45 +0400

    feat: add solution in streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from "node:fs";
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToRead.txt");
+const readableStream = fs.createReadStream(filePath, "utf-8");
+let fileData = "";
+
 const read = async () => {
-    // Write your code here 
+  readableStream.on("data", (chunk) => {
+    fileData += chunk;
+  });
+  readableStream.on("end", () => process.stdout.write(fileData + "\n"));
+  readableStream.on("error", (err) => {
+    process.stderr.write(err.message);
+  });
 };
 
 await read();
