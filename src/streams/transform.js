45f2344d3ea3d6aaabdc329bcfe01f77393efa86
Elena-Amelia <amelia013@ya.ru>
2024-01-29 20:04:50 +0400

    feat: add solution in streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from "node:stream";
+import os from "node:os";
+
+const input = process.stdin;
+const output = process.stdout;
+
+const reverseString = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(
+      null,
+      String(chunk).slice(0, -1).split("").reverse().join("") + os.EOL
+    );
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  input.pipe(reverseString).pipe(output);
 };
 
 await transform();
