a154f3880b36594e86c989308a82ba04257e434b
Elena-Amelia <amelia013@ya.ru>
2024-08-10 16:05:32 +0400

    feat: add solution for cli, cp, wt

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from "node:child_process";
+import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { stdin, stdout } from "node:process";
+
+const childPath = path.join(
+  dirname(fileURLToPath(import.meta.url)),
+  "files",
+  "script.js"
+);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn("node", [childPath, ...args]);
+  stdin.pipe(child.stdin);
+  child.stdout.pipe(stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
